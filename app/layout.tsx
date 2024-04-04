a8d31179958c656094aa09798059281b4530ed7d
Syamlal CM <syamjayaraj@gmail.com>
2024-04-04 12:43:21 +0530

    responsive issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
