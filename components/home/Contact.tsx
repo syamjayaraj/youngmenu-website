8718886b946b7b4659e41bb9b8590368d852dc59
Syamlal CM <syamjayaraj@gmail.com>
2024-04-09 11:35:11 +0530

    form validation added

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -3,40 +3,31 @@ import { submitContactForm } from "@/apiService/apiService";
 import { IContact, IContactItem, IFormData } from "@/model/models";
 import { useState } from "react";
 import RichText from "../RichText";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 
 interface customProps {
   pageData: IContact;
 }
 
+interface FormValues {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+}
+
 export default function Contact({ pageData }: customProps) {
-  const [formData, setFormData] = useState<IFormData>({
-    name: "",
-    email: "",
-    phoneNumber: "",
-    message: "",
-  });
   const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const handleInputChange = (event: any) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   const handleSubmitContactForm = async (
-    event: React.FormEvent<HTMLFormElement>
+    formData: FormValues,
+    { resetForm }: FormikHelpers<FormValues>
   ) => {
-    event.preventDefault();
+    console.log("inside");
     const response = await submitContactForm(formData);
     if (response) {
       setFormSubmitted(true);
-      setFormData({
-        name: "",
-        email: "",
-        phoneNumber: "",
-        message: "",
-      });
+      resetForm();
     }
   };
 
@@ -78,94 +69,130 @@ export default function Contact({ pageData }: customProps) {
 
           <div className="col-lg-8 offset-lg-1">
             <div className="custom-form mt-4">
-              <form onSubmit={(event) => handleSubmitContactForm(event)}>
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="form-group mt-2">
-                      <input
-                        type="text"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        value={formData.name}
-                        onChange={handleInputChange}
-                      />
+              <Formik<FormValues>
+                initialValues={{
+                  name: "",
+                  email: "",
+                  phoneNumber: "",
+                  message: "",
+                }}
+                validate={(values) => {
+                  const errors: Partial<FormValues> = {};
+                  if (!values.name) {
+                    errors.name = "Name is required";
+                  }
+                  if (!values.email) {
+                    errors.email = "Email is required";
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                      values.email
+                    )
+                  ) {
+                    errors.email = "Invalid email address";
+                  }
+                  // Add more validation rules as needed
+                  return errors;
+                }}
+                onSubmit={async (values, actions) => {
+                  await handleSubmitContactForm(values, actions);
+                  actions?.setSubmitting(false);
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    {/* Name */}
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div className="form-group mt-2">
+                          <Field
+                            type="text"
+                            name="name"
+                            className="form-control"
+                            placeholder="Name"
+                          />
+                          <ErrorMessage
+                            name="name"
+                            component="div"
+                            className="error-message text-danger"
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="form-group mt-2">
-                      <input
-                        name="email"
-                        type="email"
-                        className="form-control"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                      />
+                    {/* Email */}
+                    <div className="row">
+                      <div className="col-lg-6">
+                        <div className="form-group mt-2">
+                          <Field
+                            type="email"
+                            name="email"
+                            className="form-control"
+                            placeholder="Email"
+                          />
+                          <ErrorMessage
+                            name="email"
+                            component="div"
+                            className="error-message text-danger"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-lg-6">
+                        <div className="form-group mt-2">
+                          <Field
+                            type="text"
+                            name="phoneNumber"
+                            className="form-control"
+                            placeholder="Phone Number"
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group mt-2">
-                      <input
-                        name="phoneNumber"
-                        type="text"
-                        className="form-control"
-                        placeholder="Phone Number"
-                        value={formData.phoneNumber}
-                        onChange={handleInputChange}
-                      />
+                    {/* Message */}
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div className="form-group mt-2">
+                          <Field
+                            as="textarea"
+                            name="message"
+                            rows={4}
+                            className="form-control"
+                            placeholder="Message"
+                            style={{ resize: "none" }}
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="form-group mt-2">
-                      <textarea
-                        name="message"
-                        rows={4}
-                        className="form-control"
-                        placeholder="Message"
-                        value={formData.message}
-                        onChange={handleInputChange}
-                        style={{
-                          resize: "none",
-                        }}
-                      ></textarea>
+                    {formSubmitted && (
+                      <div className="row mt-2">
+                        <div className="col-lg-12">
+                          <p className="text-muted">
+                            <i
+                              className="mdi mdi-check-circle"
+                              style={{
+                                color: "#1cb51c",
+                                marginRight: ".3rem",
+                                fontSize: "1.2em",
+                              }}
+                            ></i>
+                            Your message has been successfully submitted
+                          </p>
+                        </div>
+                      </div>
+                    )}
+                    <div className="row mt-2">
+                      <div className="col-lg-12">
+                        <button
+                          type="submit"
+                          name="send"
+                          className="submit-button btn btn-rounded"
+                          disabled={isSubmitting}
+                        >
+                          {isSubmitting ? "Sending..." : "Send Message"}
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                {formSubmitted && (
-                  <div className="row mt-2">
-                    <div className="col-lg-12">
-                      <p className="text-muted">
-                        <i
-                          className="mdi mdi-check-circle"
-                          style={{
-                            color: "#1cb51c",
-                            marginRight: ".3rem",
-                            fontSize: "1.2em",
-                          }}
-                        ></i>
-                        Your message has been successfully submitted
-                      </p>
-                    </div>
-                  </div>
+                  </Form>
                 )}
-                <div className="row mt-2">
-                  <div className="col-lg-12">
-                    <button
-                      type="submit"
-                      name="send"
-                      className="submit-button btn btn-rounded"
-                    >
-                      Send Message
-                    </button>
-                  </div>
-                </div>
-              </form>
+              </Formik>
             </div>
           </div>
         </div>
